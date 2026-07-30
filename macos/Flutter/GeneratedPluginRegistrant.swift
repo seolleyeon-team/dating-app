8b7824150627704441c82458035db3d2a042ef9c
chaehong0 <chaehongi25@gmail.com>
2026-07-31 05:52:05 +0900

    chore: regenerate desktop plugin registrants for audioplayers
    flutter pub get rewrote the linux, macos and windows registrants when
    audioplayers was added for the icebreaker sound effects, but the generated
    files were left out of that commit.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import app_links
+import audioplayers_darwin
 import cloud_firestore
 import cloud_functions
 import file_selector_macos
@@ -26,6 +27,7 @@ import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FirebaseFunctionsPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
